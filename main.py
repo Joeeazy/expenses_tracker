c36c4efb834e45aefe7fc1b1ff607118913641bb
Joeeazy <joeeazy34@gmail.com>
2024-07-12 18:37:26 +0300

    function to get all stored data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data_entry import get_description, get_amount, get_category, get_date
 class CSV:
     CSV_FILE = "financial_data.csv"
     COLUMNS = ["DATE", "AMOUNT", "CATEGORY", "DESCRIPTION"]
+    FORMAT = "%d-%m-%y"
 
     # will have access to the class itself
     @classmethod
@@ -37,6 +38,33 @@ class CSV:
             writer.writerow(new_entry)
         print("Entries added successfully")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        dataframe = panda.read_csv(cls.CSV_FILE)
+        # convert date to date column object
+        dataframe["DATE"] = panda.to_datetime(dataframe["DATE"], format=CSV.FORMAT)
+        start_date=datetime.strptime(start_date, CSV.FORMAT)
+        end_date=datetime.strptime(end_date, CSV.FORMAT)
+
+        mask = (dataframe["DATE"] >= start_date) & (dataframe["DATE"] <= end_date)
+
+        filtered_dataframe = dataframe.loc(mask)
+
+        if filtered_dataframe.empty:
+            print("No transactions found were found in the given date range")
+        else:
+            print(f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+
+        print(filtered_dataframe.to_string(index=False, formatters={"DATE": lambda x: x.strftime(CSV.FORMAT)}))
+
+        total_income = filtered_dataframe[filtered_dataframe["CATEGORY"] == "Income"]["AMOUNT"].sum()
+        total_expenses = filtered_dataframe[filtered_dataframe["CATEGORY"] == "Expenses"]["AMOUNT"].sum()
+        print("\nSummary")
+        print(f"Total Income: ${total_income:.2f}")
+        print(f"Total Expenses: ${total_expenses:.2f}")
+
+        return filtered_dataframe
+
 # function to call get_entries in order for data collection
 def add():
     CSV.initialize_csv()
@@ -47,4 +75,5 @@ def add():
 
     CSV.add_entries(date, amount, category, description)
 
-add()
+#add()
+CSV.get_transactions("01-01-2023", "30-12-2024")
