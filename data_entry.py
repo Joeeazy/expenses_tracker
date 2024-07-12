75c1dc0de97a9d39b9b16ef88e63b2458fe1b776
Joeeazy <joeeazy34@gmail.com>
2024-07-12 17:05:28 +0300

    functions to get user data

---
+++ data_entry.py
@@ -0,0 +1,16 @@
+# This file handles all functions that handles
+#  getting information from the user in the cl
+
+from datetime import datetime
+
+def get_date(prompt, allow_default=False):
+    pass
+
+def get_amount():
+    pass
+
+def get_category():
+    pass
+
+def get_description():
+    pass
