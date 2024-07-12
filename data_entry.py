c6e1bf64a689328fe5095969b1b23a1c5af37aa1
Joeeazy <joeeazy34@gmail.com>
2024-07-12 17:14:49 +0300

    enter date prompt function done

--- data_entry.py
+++ data_entry.py
@@ -3,8 +3,23 @@
 
 from datetime import datetime
 
+date_format = "%d-%m-%y"
+
 def get_date(prompt, allow_default=False):
-    pass
+    date_str = input(prompt)
+    # if user doesnt input the date generate date in string format
+    if allow_default and not date_str:
+        return datetime.today().strftime(date_format)
+
+    # check for date validity and parse it to the required format
+    try:
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
+    except ValueError:
+        print("Invalid date format. Please Enter the date in dd-mm-yyyy format")
+        # recurse for proper date innput
+        return get_date(prompt, allow_default=False)
+
 
 def get_amount():
     pass
